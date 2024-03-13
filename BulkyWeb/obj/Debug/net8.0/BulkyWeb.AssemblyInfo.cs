8662edf82f831aefb1dd005b9e78d25cf9ae4e3a
Wouter Malan <WouterM@gobid.co.za>
2024-03-13 11:16:58 +0200

    Update BulkyWeb project binaries and fix CategoryController

--- BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1d98348b24b3746a694663f37cee848657cabbb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f43fe1ee1e45ce13daafc605c3a44d074d1b649")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
