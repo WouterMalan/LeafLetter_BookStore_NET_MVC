0ad4bf252505bfd0fe204a87e3b1c61c4010a06a
WouterMalan <woutermalan7909@gmail.com>
2024-03-21 10:23:43 +0200

    Update binary files in Debug folder

--- Bulky.DataAccess/obj/Debug/net8.0/Bulky.DataAccess.AssemblyInfo.cs
+++ Bulky.DataAccess/obj/Debug/net8.0/Bulky.DataAccess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bulky.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7668877693257811c02d56c17e4db2558c47f747")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3752d30501d364cfd7348103f2ca95db5cbea683")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bulky.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bulky.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Bulky.Models/obj/Debug/net8.0/Bulky.Models.AssemblyInfo.cs
+++ Bulky.Models/obj/Debug/net8.0/Bulky.Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bulky.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7668877693257811c02d56c17e4db2558c47f747")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3752d30501d364cfd7348103f2ca95db5cbea683")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bulky.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bulky.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
