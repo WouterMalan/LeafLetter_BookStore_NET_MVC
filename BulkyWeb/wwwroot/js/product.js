7d5c38e1d386683ece898cb464306d71696980fb
Wouter Malan <WouterM@gobid.co.za>
2024-03-26 15:06:02 +0200

    Update column name in product.js

--- BulkyWeb/wwwroot/js/product.js
+++ BulkyWeb/wwwroot/js/product.js
@@ -7,7 +7,7 @@ function loadDataTable() {
     dataTable = $('#productTable').DataTable( {
         "ajax": {url:'/admin/product/getall'},
         "columns": [
-            { data: 'dfgdfg', width: '25%' },
+            { data: 'title', width: '25%' },
             { data: 'isbn', width: '15%' },
             { data: 'listPrice', width: '10%' },
             { data: 'author', width: '20%' },
