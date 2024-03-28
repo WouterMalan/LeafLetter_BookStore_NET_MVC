928be93eb7e794fc808cdc641d8c4f949afb0ac1
Wouter Malan <WouterM@gobid.co.za>
2024-03-28 11:23:21 +0200

    Updated the table to a data table and fixed the edit funcitonality and all the display properties of the table in product.js

--- BulkyWeb/wwwroot/js/product.js
+++ BulkyWeb/wwwroot/js/product.js
@@ -5,14 +5,41 @@ $(document).ready(function () {
 // Load DataTable
 function loadDataTable() {
     dataTable = $('#productTable').DataTable({
-        "ajax": {url:'/admin/product/getall'},
+        "ajax": {
+            url: '/admin/product/getall'
+            // dataSrc: function(json) {
+            //     console.log(json);
+            //     return json;
+            // }
+        },
         "columns": [
             { data: 'title', width: '25%' },
             { data: 'isbn', width: '15%' },
             { data: 'listPrice', width: '10%' },
-            { data: 'author', width: '20%' },
-            { data: 'category.name', width: '15%' }
+            { data: 'author', width: '15%' },
+            {
+                data: 'category', 
+                width: '10%',
+                "render": function(data, type, row) {
+                    return data ? data.name : '';
+                }
+            },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `
+                        <div class="w-75 btn-group" role="group">
+                            <a href="/Admin/Product/Upsert/${data}" class="btn btn-success mx-2" style="cursor:pointer;">
+                                <i class="bi bi-pencil-square"></i> Edit
+                            </a>
+                            <a onclick=Delete("/Admin/Product/Delete/${data}") class="btn btn-danger mx-2" style="cursor:pointer;">
+                                <i class="bi bi-trash-fill"></i> Delete
+                            </a>
+                        </div>
+                    `;
+                },
+                "width": "25%"
+            }
         ]
     });
 }
-
