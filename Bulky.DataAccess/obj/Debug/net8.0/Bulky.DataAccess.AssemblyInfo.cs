becf1917594cfec226b99e55aed8e8add92792ca
Wouter Malan <WouterM@gobid.co.za>
2024-04-02 15:26:20 +0200

    Update binary files in BulkyWeb, Bulky.Models, Bulky.Utility, and Bulky.DataAccess

--- Bulky.DataAccess/obj/Debug/net8.0/Bulky.DataAccess.AssemblyInfo.cs
+++ Bulky.DataAccess/obj/Debug/net8.0/Bulky.DataAccess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bulky.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81fb321748cc6542790a483fa3fa049296102d74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea230a3c661a4f49dc7c836e417963c7667717a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bulky.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bulky.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
