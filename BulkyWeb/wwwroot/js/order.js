31300bcb4a88ec853917bbdb8386adaba7b3835e
Wouter Malan <WouterM@gobid.co.za>
2024-04-23 10:29:45 +0200

    Added payment status filters to OrderController and order.js

--- BulkyWeb/wwwroot/js/order.js
+++ BulkyWeb/wwwroot/js/order.js
@@ -1,14 +1,38 @@
 var dataTable;
 
 $(document).ready(function () {
+    var url = window.location.search;
+
+    if (url.includes("inprocess")) {
+        loadDataTable("inprocess");
+    }
+    else {
+        if (url.includes("completed")) {
+            loadDataTable("completed");
+        }
+        else {
+            if (url.includes("pending")) {
+                loadDataTable("pending");
+            }
+            else {
+                if (url.includes("approved")) {
+                    loadDataTable("approved");
+                }
+                else {
+                    loadDataTable("all");
+                }
+            }
+        }
+    }
+
     loadDataTable();
 });
 
 // Load DataTable
-function loadDataTable() {
+function loadDataTable(status) {
     dataTable = $('#productTable').DataTable({
         "ajax": {
-            url: '/admin/order/getall'
+            url: '/admin/order/getall?status=' + status
             // dataSrc: function(json) {
             //     console.log(json);
             //     return json;
@@ -16,9 +40,9 @@ function loadDataTable() {
         },
         "columns": [
             { data: 'id', width: '25%' },
-            { data: 'name', width: '15%' },
-            { data: 'phoneNumber', width: '10%' },
-            { data: 'applicationUser.email', width: '15%' },
+            { data: 'name', width: '20%' },
+            { data: 'phoneNumber', width: '20%' },
+            { data: 'applicationUser.email', width: '20%' },
             { data: 'orderStatus', width: '10%' },
             { data: 'orderTotal', width: '10%' },
             {
@@ -39,7 +63,7 @@ function loadDataTable() {
                         </div>
                     `;
                 },
-                "width": "25%"
+                "width": "10%"
             }
         ]
     });
