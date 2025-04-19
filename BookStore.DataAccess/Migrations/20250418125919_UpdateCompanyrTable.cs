85a31c2c4c3c412bd4fa4f1cda5bdc8c655cd845
Wouter <woutermalan7909@gmail.com>
2025-04-19 13:15:33 +0200

    Add initial project files and configuration for BookStoreWeb; set up logging and database connection settings

---
+++ BookStore.DataAccess/Migrations/20250418125919_UpdateCompanyrTable.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bulky.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCompanyrTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "State",
+                table: "AspNetUsers",
+                newName: "Province");
+
+            migrationBuilder.UpdateData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "State",
+                value: "NY");
+
+            migrationBuilder.UpdateData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "State",
+                value: "NY");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Province",
+                table: "AspNetUsers",
+                newName: "State");
+
+            migrationBuilder.UpdateData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "State",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "State",
+                value: null);
+        }
+    }
+}
